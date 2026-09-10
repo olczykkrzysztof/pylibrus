4754f06e4f098660b5b51b962a69f68ea9f10f68
Krzysztof Olczyk <olczyk.krzysztof@gmail.com>
2026-09-10 17:07:06 +0200

    Process users in a random order

--- src/pylibrus/pylibrus.py
+++ src/pylibrus/pylibrus.py
@@ -1287,11 +1287,13 @@ def main():
     if args.test_notify:
         return send_test_notification(pylibrus_config, librus_users[0])
 
-    for i, librus_user in enumerate(librus_users):
+    users_to_handle = librus_users.copy()
+    random.shuffle(users_to_handle)
+    while len(users_to_handle) > 0:
+        librus_user = users_to_handle.pop(0)
         handle_user(pylibrus_config, librus_user, dry_run=args.dry)
-        if i != len(librus_users) - 1:
+        if len(users_to_handle) > 0:
             time.sleep(pylibrus_config.sleep_between_librus_users)
-
 
 if __name__ == "__main__":
     sys.exit(main())
